35bff0e11f49afdc81390814af5d4d536c19cc54
jontorregano <jontorregano@gmail.com>
2017-05-16 12:54:30 -0500

    Initial Commit

---
+++ FoodSiteMVC/FoodSiteMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodSiteMVC.MvcApplication" Language="C#" %>
---
+++ FoodSiteMVC/FoodSiteMVC/Models/Orders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace FoodSiteMVC.Models
+{
+    public class Orders
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class OrdersDBContext : DbContext
+    {
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
